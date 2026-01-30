18641adbb4eb4f5ffcd9c53ee750a76d11fef9d6
ItencY <itency@outlook.com>
2026-01-30 09:12:36 +0300

    impliment move press key a and d

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
